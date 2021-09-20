99f24717106cdb2bad08727f090a905c3f8cbd02
desencrypt <danielghack@gmail.com>
2021-09-20 17:11:57 +0200

    primera version

---
+++ pdmdm/src/Main2.java
@@ -0,0 +1,13 @@
+public class Main2 {
+    public static void main(String[] args)
+    {
+        String sNombre = "albertofernandezsanchez";
+        //obtener el numero de letras "e" en un texto
+        for (int i = 0; i < sNombre.length(); i++)
+        {
+
+
+        }
+
+    }
+}
